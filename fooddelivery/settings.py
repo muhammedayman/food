ce25bfdeef0c3538f9a21fe8f7e5665ab0537141
aymen <aymen@akbartravels.in>
2021-01-20 23:38:26 +0530

    allowed host

--- fooddelivery/settings.py
+++ fooddelivery/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY =env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.10','192.168.1.5','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['192.168.1.10','192.168.1.5','127.0.0.1','localhost','blog4.tech','127.0.0.1']
 
 
 # Application definition
